6b9904f0616555878dfd44a26d061ad5773ca0be
guinala <guinala@alumni.uv.es>
2026-07-20 16:47:29 +0200

    SO de cartas
    Signed-off-by: guinala <guinala@alumni.uv.es>

--- Assets/Scripts/ScriptableObjects/CardData.cs
+++ Assets/Scripts/ScriptableObjects/CardData.cs
@@ -3,10 +3,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName= "CardData", menuName = "ScriptableObjects/CardData")]
 public class CardData : ScriptableObject
 {
+    [Header("General")]
     public string cardName;
     public string description;
     public int actionCost;
     public Sprite illustration;
+    public bool multipleTurns;
+    [HideInInspector] public int remainingTurns;
+
+    [Header("Stats")]
     public int attackPower;
     public int healPower;
+    public int poisonPower;
+    public int poisonTurns;
+    public int defensePower;
 }
